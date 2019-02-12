ba506480e2f09ee60a284fd3d02a94a0f7a29a65
Daniel Quiñones Lopez <dquinoneslopez@gmail.com>
2019-02-12 12:22:17 +0100

    Primer commit

---
+++ app.js
@@ -0,0 +1,28 @@
+// Requires
+var express = require('express');
+var mongoose = require('mongoose');
+
+// Inicializar variables
+var app = express();
+
+// Conexión a la DB
+mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
+
+    if (err) throw err;
+    console.log('Base de datos: \x1b[32m%s\x1b[0m', 'online');
+
+});
+
+// Rutas
+app.get('/', (req, res, next) => {
+    // https://es.wikipedia.org/wiki/Anexo:C%C3%B3digos_de_estado_HTTP
+    res.status(200).json({
+        ok: true,
+        mensaje: 'Petición realizada correctamente.'
+    });
+});
+
+// Escuchar peticiones
+app.listen(3000, () => {
+    console.log('Express server puerto 3000: \x1b[32m%s\x1b[0m', 'online');
+});
